d50c7e718b13b67fd1ddb41b01626524f1241fdc
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-16 08:48:26 +0900

    init

---
+++ data/data.js
@@ -0,0 +1,95 @@
+export const data = {
+  "title": "React list 구현",
+  "data": [
+      {
+          "id": 1,
+          "name": '권준',
+          "level": 2
+      },
+      {
+          "id": 2,
+          "name": '김규민',
+          "level": 1
+      },
+      {
+          "id": 3,
+          "name": '김민찬',
+          "level": 5
+      },
+      {
+          "id": 4,
+          "name": '문찬영',
+          "level": 4
+      },
+      {
+          "id": 5,
+          "name": '박신호',
+          "level": 2
+      },
+      {
+          "id": 6,
+          "name": '박종혁',
+          "level": 5
+      },
+      {
+          "id": 7,
+          "name": '박현우',
+          "level": 1
+      },
+      {
+          "id": 8,
+          "name": '서하늘',
+          "level": 4
+      },
+      {
+          "id": 9,
+          "name": '이재원',
+          "level": 4
+      },
+      {
+          "id": 10,
+          "name": '임준건',
+          "level": 5
+      },
+      {
+          "id": 11,
+          "name": '장민우',
+          "level": 5
+      },
+      {
+          "id": 12,
+          "name": '전민수',
+          "level": 2
+      },
+      {
+          "id": 13,
+          "name": '전찬하',
+          "level": 2
+      },
+      {
+          "id": 14,
+          "name": '정민건',
+          "level": 1
+      },
+      {
+          "id": 15,
+          "name": '정유권',
+          "level": 5
+      },
+      {
+          "id": 16,
+          "name": '조수빈',
+          "level": 3
+      },
+      {
+          "id": 17,
+          "name": '하승수',
+          "level": 5
+      },
+      {
+          "id": 18,
+          "name": '박형진',
+          "level": undefined
+      },
+  ]
+}
