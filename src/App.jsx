68a17e9fd398999215e70a1af11742261981fefd
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-16 12:31:48 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
+import { data as DATA } from '../data/data';
+import Search from './components/Search';
+import List from './components/List';
 import './App.css'
+import SortById from './components/SortById';
+import SortByLevel from './components/SortByLevel';
+import SortByName from './components/SortByName';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, title } = DATA;
+  const [listData, setListData] = useState(data);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    console.log(listData);
+    
+  }, [listData]);
+
+  const ascendingById = () => {
+    const copyedState = structuredClone(listData);
+
+    copyedState.sort((a, b) => a.id - b.id );
+    setListData(copyedState); 
+  }
+
+  const descendingById = () => {
+    const copyedState = structuredClone(listData);
+
+    copyedState.sort((a, b) => b.id - a.id );
+    setListData(copyedState); 
+  }
+
+  const ascendingByLevel = () => {
+    const copyedState = structuredClone(listData);
+
+    copyedState.sort((a, b) => a.level - b.level );
+    
+    const filtered = copyedState.reduce((acc, el) => {
+      if (!el.level) {
+        return [el, ...acc];
+      }
+      return [...acc, el];
+    }, []);
+
+    setListData(filtered);
+  }
+
+  const ascendingByName = () => {
+    const copyedState = structuredClone(listData);
+
+    copyedState.sort((a, b) => a.name.localeCompare(b.name) );
+    
+    setListData(copyedState);
+  }
+
+  const descendingByName = () => {
+    const copyedState = structuredClone(listData);
+
+    copyedState.sort((a, b) => b.name.localeCompare(a.name) );
+    setListData(copyedState); 
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{title}</h1>
+      <SortById ascending={ascendingById} descending={descendingById} />
+      <SortByLevel ascending={ascendingByLevel}/>
+      <SortByName ascending={ascendingByName} descending={descendingByName} />
+      
+      <Search search={search} setSearch={setSearch}/>
+      <List search={search} listData={listData} />
     </>
   )
 }
